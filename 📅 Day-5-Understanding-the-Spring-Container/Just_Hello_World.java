53a530a0a3091ff1265a7ea5e7079026bdf210cb
Vinay kumar <vinaykumar860964@gmail.com>
2024-08-13 11:33:56 +0530

    Updated

---
+++ 📅 Day-5-Understanding-the-Spring-Container/Just_Hello_World.java
@@ -0,0 +1,6 @@
+
+public class Just_Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ 📅 Day-6-Introduction-to-AOP/Just_Hello_World.java
@@ -0,0 +1,7 @@
+
+public class Just_Hello_World {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
