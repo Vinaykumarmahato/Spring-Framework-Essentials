599790501ec6cd601fb3f19b670cf8e6ae3a1262
Vinay kumar <116486037+Vinaykumarmahato@users.noreply.github.com>
2024-10-05 06:46:12 +0000

    Spring Starter project added

---
+++ hello-world/src/test/java/com/example/hello_world/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
